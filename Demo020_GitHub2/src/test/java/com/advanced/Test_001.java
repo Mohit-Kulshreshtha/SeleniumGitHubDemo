a12e24f75731967bd92a7c61cd867bd9607355ac
mohit.kulshreshtha <mohit.kulshreshtha@BDC4-L-52971R5.dir.svc.accenture.com>
2018-05-04 14:54:47 +0530

    Try

---
+++ Demo020_GitHub2/src/test/java/com/advanced/Test_001.java
@@ -0,0 +1,30 @@
+package com.advanced;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class Test_001 {
+
+	WebDriver driver;
+	
+	@BeforeTest
+	public void setUp() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Full Backup\\Official\\Testing tools Skill Set\\Selenium\\Selenium Installables Files\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+	}
+	
+	@AfterTest
+	public void tearDown()
+	{
+		driver.quit();
+	}
+	
+	@Test
+	public void openBrowser()
+	{
+		driver.get("http://store.demoqa.com/");
+        System.out.println("Current page title: "+driver.getTitle());
+	}
+}
